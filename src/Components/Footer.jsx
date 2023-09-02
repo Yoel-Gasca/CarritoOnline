4b299c78b734fb0fa5560fd616be40146f736cfa
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-09-01 20:00:47 -0600

    Extra documentation

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ import './Footer.css'
 export function Footer () {
     // const {filters} = useFilters()
     
+    // Crea un Footer para la etapa de Desarrollo
     return(
         <footer className='footer'>
             <h4>Desarrollado con React ⚛️</h4>
